4b7aa003c94a8cea1083e5a6ae89b24ea1b82aa2
Joe <hutcheson2001@yahoo.com>
2022-01-15 16:11:07 -0500

    commit tree 39c8e819b571342dc0315c0f03fef090abd05a4a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+ 
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+    }
+
+function overwriteBestCustomer(){
+bestCustomer = 'maybe bob';
+return bestCustomer;
+    }
+
+
+const leastFavoriteCustomer = "Trump";
+
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = "Biden";
+}
